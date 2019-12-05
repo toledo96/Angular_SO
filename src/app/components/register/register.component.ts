c9f8a03bfd202c3c5b9afe19b5f6a5e0d8360c78
andres <andresboy_123@hotmail.com>
2019-12-04 22:20:45 -0600

    aqui vamos otra vez x7

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -5,15 +5,15 @@ import { HttpClient } from 'selenium-webdriver/http';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
-  selector: 'register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+	selector: 'register',
+	templateUrl: './register.component.html',
+	styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
 
-  public title: string;
+	public title: string;
 	public user: User;
-	public status:string;
+	public status: string;
 	public identity;
 	public token;
 	public _http: HttpClient;
@@ -21,66 +21,69 @@ export class RegisterComponent implements OnInit {
 
 
 
-  constructor(
+	constructor(
 		private _route: ActivatedRoute,
 		private _router: Router,
 		private apiService: ApiService
-		){
-		this.user = new User("","","","","","","","false");
+	) {
+		this.user = new User("", "", "", "", "", "", "", "false");
 
 
 	}
 
-  ngOnInit() {
-	  this.apiService.getCarreras().subscribe(response => {
-		  console.log(response);
-		  this.carreras = response;
-	  })
-	//   this.carreras = [
-	// 	{
-	// 		"nombre": "Ingeniería ambiental",
-			
-	// 	},
-	// 	{
-			
-	// 		"nombre": "Ingeniería en desarrollo de Software",
-			
-	// 	},
-	// 	{
-			
-	// 		"nombre": "Ingeniería Biomedica",
-			
-	// 	},
-	// 	{
-			
-	// 		"nombre": "Ingeniería Agroindustrial",
-			
-	// 	},
-	// ]
-	  console.log(this.carreras);
-  }
-
-  
-
-  setCarrera(carrera){
-	  console.log(carrera);
-	  this.user.carrera = carrera;
-  }
-  createUser(){
-	this.apiService.create(this.user).subscribe(
-		response => {
+	ngOnInit() {
+		this.apiService.getCarreras().subscribe(response => {
 			console.log(response);
-			
-		},
-		error => {
-			console.log(<any>error);
-		}
-	); 
-  }
+			this.carreras = response;
+		})
+		//   this.carreras = [
+		// 	{
+		// 		"nombre": "Ingeniería ambiental",
 
-  volver(){
-	this._router.navigate(['/home']);
-  }		
+		// 	},
+		// 	{
+
+		// 		"nombre": "Ingeniería en desarrollo de Software",
+
+		// 	},
+		// 	{
+
+		// 		"nombre": "Ingeniería Biomedica",
+
+		// 	},
+		// 	{
+
+		// 		"nombre": "Ingeniería Agroindustrial",
+
+		// 	},
+		// ]
+		console.log(this.carreras);
+	}
+
+
+
+	setCarrera(carrera) {
+		console.log(carrera);
+		this.user.carrera = carrera;
+	}
+	createUser() {
+		this.apiService.create(this.user).subscribe(
+			response => {
+				console.log(response);
+				////////////////////////
+				this._router.navigate(['/home']);
+				/////////////////////////
+
+			},
+			error => {
+				console.log(<any>error);
+			}
+		);
+	}
+
+	volver() {
+		this._router.navigate(['/home']);
+	}
 
 
 }
